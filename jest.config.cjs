809462943fee39612305642a95518508c400a582
jihwooon <jihwooon@gmail.com>
2023-12-07 11:42:50 +0900

    first init

---
+++ jest.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  "moduleFileExtensions": [
+    "js",
+    "json",
+    "ts"
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    'dist',
+  ],
+  "rootDir": "src",
+  "testRegex": ".*\\.(test|spec)\\.js$",
+  "collectCoverageFrom": [
+    "**/*.js"
+  ],
+  "coverageDirectory": "../coverage",
+  "testEnvironment": "node",
+  testPathIgnorePatterns: [
+    '/node_modules/',
+  ],
+  "setupFiles": [
+    "jest-plugin-context/setup"
+  ]
+};
